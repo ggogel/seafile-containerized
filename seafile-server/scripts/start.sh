9b55172cc7cd617b4774ad5838209df9092e3cc3
Gerrit Gogel <gerrit@gogel.me>
2021-01-26 22:42:14 +0100

    refactor start.sh and add graceful shutdown

--- seafile-server/scripts/start.sh
+++ seafile-server/scripts/start.sh
@@ -1,12 +1,45 @@
 #!/bin/bash
 
-/scripts/create_data_links.sh
+function sigterm {
+    stop_socat
+    stop_server
+    exit 0
+}
 
-python3 /scripts/start.py &
+function start_server {
+    /scripts/create_data_links.sh
+    python3 /scripts/start.py
+}
 
-while [ ! -S /opt/seafile/seafile-server-latest/runtime/seafile.sock ]; do
-    echo "Waiting for SeaRPC socket..."
-    sleep 1
-done
+function stop_server {
+    echo "Stopping seafile server..."
+    /opt/seafile/seafile-server-latest/seafile.sh stop
+}
 
-socat -d -d TCP-LISTEN:8001,fork,reuseaddr UNIX:/opt/seafile/seafile-server-latest/runtime/seafile.sock,forever
+function start_socat {
+    while [ ! -S /opt/seafile/seafile-server-latest/runtime/seafile.sock ]; do
+        echo "Waiting for SeaRPC socket..."
+        sleep 1
+    done
+    socat -d -d TCP-LISTEN:8001,fork,reuseaddr UNIX:/opt/seafile/seafile-server-latest/runtime/seafile.sock,forever
+}
+
+function stop_socat {
+    echo "Stopping socat..."
+    ps aux | grep '[s]ocat' | awk '{print $2}' | xargs kill -15 > /dev/null
+    sleep 2
+    if [ -z $(ps aux | grep '[s]ocat')]; then
+        ps aux | grep '[s]ocat' | awk '{print $2}' | xargs kill -9 > /dev/null
+    fi
+}
+
+function keep_running {
+    while true; do
+        tail -f /dev/null & wait ${!}
+    done
+}
+
+trap 'sigterm' SIGTERM
+start_server &
+start_socat &
+keep_running
