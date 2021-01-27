c2bedff29d79814962a0cbdd0bffaa65668f4433
Gerrit Gogel <gerrit@gogel.me>
2021-01-28 00:39:18 +0100

    start.sh: refactored, add first admin init

--- seahub/scripts/start.sh
+++ seahub/scripts/start.sh
@@ -1,9 +1,24 @@
 #!/bin/bash
 
-/scripts/create_data_links.sh
+function start_seahub {
+    /scripts/create_data_links.sh
+    echo "{ \"email\": \"${SEAFILE_ADMIN_EMAIL}\",\"password\": \"${SEAFILE_ADMIN_PASSWORD}\"}" | tee /opt/seafile/conf/admin.txt
+    #/opt/seafile/seafile-server-latest/seahub.sh start
+    python3 /opt/seafile/seafile-server-latest/check_init_admin.py
+    python3 /opt/seafile/seafile-server-latest/seahub/manage.py runserver 0.0.0.0:8000
+}
 
-mkdir -p /opt/seafile/seafile-server-latest/runtime
-socat -d -d UNIX-LISTEN:/opt/seafile/seafile-server-latest/runtime/seafile.sock,fork TCP:seafile-server:8001,forever,keepalive,keepidle=10,keepintvl=10,keepcnt=2  &
+function start_socat {
+    mkdir -p /opt/seafile/seafile-server-latest/runtime
+    socat -d -d UNIX-LISTEN:/opt/seafile/seafile-server-latest/runtime/seafile.sock,fork TCP:seafile-server:8001,forever,keepalive,keepidle=10,keepintvl=10,keepcnt=2
+}
 
-python3 /opt/seafile/seafile-server-latest/seahub/manage.py runserver 0.0.0.0:8000
+function keep_running {
+    while true; do
+        tail -f /dev/null & wait ${!}
+    done
+}
 
+start_socat &
+start_seahub &
+keep_running
