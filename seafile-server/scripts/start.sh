8b658bd7555bdeb749396f60705fcb3b01dca8e1
Gerrit Gogel <gerrit@gogel.me>
2021-02-01 13:20:19 +0100

    move functions to utils.sh

--- seafile-server/scripts/start.sh
+++ seafile-server/scripts/start.sh
@@ -1,41 +1,6 @@
 #!/bin/bash
 
-function sigterm {
-    stop_socat
-    stop_server
-    exit 0
-}
-
-function start_server {
-    /scripts/create_data_links.sh
-    python3 /scripts/start.py
-}
-
-function stop_server {
-    echo "Stopping seafile server..."
-    /opt/seafile/seafile-server-latest/seafile.sh stop
-}
-
-function start_socat {
-    echo "Waiting for SeaRPC socket..."
-    while [ ! -S /opt/seafile/seafile-server-latest/runtime/seafile.sock ]; do
-        sleep 1
-    done
-    socat -d -d TCP-LISTEN:8001,fork,reuseaddr UNIX:/opt/seafile/seafile-server-latest/runtime/seafile.sock,forever
-}
-
-function stop_socat {
-    echo "Stopping socat..."
-    pkill -15 -f socat
-    sleep 5
-    pkill -9 -f socat
-}
-
-function keep_running {
-    while true; do
-        tail -f /dev/null & wait ${!}
-    done
-}
+source /scripts/utils.sh
 
 trap 'sigterm' SIGTERM
 start_server &
