b7ef93b362ddb1417290501b08839c9eab995644
Gerrit Gogel <gerrit@gogel.me>
2021-02-04 00:11:54 +0100

    move correction of gunicorn bind setting from seafile-server to seahub

--- seahub/scripts/start.sh
+++ seahub/scripts/start.sh
@@ -3,7 +3,7 @@
 function init_seahub {
     /scripts/create_data_links.sh
     echo "{ \"email\": \"${SEAFILE_ADMIN_EMAIL}\",\"password\": \"${SEAFILE_ADMIN_PASSWORD}\"}" >/opt/seafile/conf/admin.txt
-    python3 /opt/seafile/seafile-server-latest/check_init_admin.py
+    sed -i 's@bind =.*@bind = "0.0.0.0:8000"@' /opt/seafile/conf/gunicorn.conf.py
 }
 
 function start_seahub {
